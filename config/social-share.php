19b1afd8a67ef9d52961b5c71f2308ffabb5ae7c
zaydolas74 <r0934607@student.thomasmore.be>
2024-07-09 02:08:45 +0200

    plugin

---
+++ config/social-share.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    '*' => [
+        'enableCache' => true,
+        'cacheDuration' => 86400,
+        'friendlyCount' => true,
+        'minShareCount' => null,
+        'useModalForShare' => true,
+        'buttonAttributes' => [],
+        'iconWrapperAttributes' => [],
+        'labelAttributes' => [],
+        'labelWrapperAttributes' => [],
+        'contentAttributes' => [],
+        'providers' => [],
+    ]
+];
